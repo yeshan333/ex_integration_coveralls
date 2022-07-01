73daa2d04c00a3f2f0e24f12f49e853d4ab0b025
yeshan <yeshan.ye@alibaba-inc.com>
2022-07-01 15:08:45 +0800

    fix: add log point

--- lib/ex_integration_coveralls/cov_stats_router.ex
+++ lib/ex_integration_coveralls/cov_stats_router.ex
@@ -1,16 +1,22 @@
 defmodule ExIntegrationCoveralls.CovStatsRouter do
   use Plug.Router
+  require Logger
 
   plug :match
   plug Plug.Parsers, parsers: [:json], json_decoder: Poison
   plug :dispatch
 
+  get "/ping" do
+    send_resp(conn, 200, "pong!")
+  end
+
   get "/stats" do
     stats = ExIntegrationCoveralls.get_app_total_cov("explore_ast_app")
-    resp = ExIntegrationCoveralls.Json.generate_json_output(%{
+    stats_json = ExIntegrationCoveralls.Json.generate_json_output(%{
       coverage: stats
     })
-    send_resp(conn, 200, resp)
+    Logger.warn("stats: #{inspect(stats)}, resp #{stats_json}")
+    send_resp(conn, 200, stats_json)
   end
 
   match _ do
