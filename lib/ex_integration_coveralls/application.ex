17751914525c3d088839fe59fdaefe314f16708c
yeshan <yeshan.ye@alibaba-inc.com>
2022-07-01 15:49:16 +0800

    feat: add more http api to export cov stats

--- lib/ex_integration_coveralls/application.ex
+++ lib/ex_integration_coveralls/application.ex
@@ -1,19 +1,16 @@
 defmodule ExIntegrationCoveralls.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
+  @moduledoc """
+
+  """
 
   use Application
   alias ExIntegrationCoveralls.CovStatsRouter
 
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
       start_http_worker()
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: ExIntegrationCoveralls.Supervisor]
     Supervisor.start_link(children, opts)
   end
--- lib/ex_integration_coveralls/cov_stats_router.ex
+++ lib/ex_integration_coveralls/cov_stats_router.ex
@@ -1,6 +1,9 @@
 defmodule ExIntegrationCoveralls.CovStatsRouter do
   use Plug.Router
   require Logger
+  alias ExIntegrationCoveralls.Json
+  alias ExIntegrationCoveralls.PathReader
+  alias ExIntegrationCoveralls.CoverageCiPoster
 
   plug :match
   plug Plug.Parsers, parsers: [:json], json_decoder: Poison
@@ -10,16 +13,39 @@ defmodule ExIntegrationCoveralls.CovStatsRouter do
     send_resp(conn, 200, "pong!")
   end
 
-  get "/stats" do
-    stats = ExIntegrationCoveralls.get_app_total_cov("explore_ast_app")
-    stats_json = ExIntegrationCoveralls.Json.generate_json_output(%{
-      coverage: stats
+  post "/cov/start" do
+    {status, body} =
+      case conn.body_params do
+        %{"app_name" => app_name} -> {200, ExIntegrationCoveralls.start_app_cov(app_name)}
+        _ -> {400, "bad request"}
+      end
+
+    send_resp(conn, status, Json.generate_json_output(body))
+  end
+
+  get "/cov/total/:app_name" do
+    total_cov = ExIntegrationCoveralls.get_app_total_cov(app_name)
+    body = Json.generate_json_output(%{
+      coverage: total_cov
     })
-    Logger.warn("stats: #{inspect(stats)}, resp #{stats_json}")
-    send_resp(conn, 200, stats_json)
+
+    Logger.info("stats: #{inspect(total_cov)}, resp: #{body}")
+    send_resp(conn, 200, body)
+  end
+
+  get "/cov/report/:app_name" do
+    {run_time_source_lib_abs_path, compile_time_source_lib_abs_path, _} =
+      PathReader.get_app_cover_path(app_name)
+
+    stats =
+      CoverageCiPoster.get_coverage_stats(compile_time_source_lib_abs_path, run_time_source_lib_abs_path)
+      |> CoverageCiPoster.stats_transformer
+
+    body = Json.generate_json_output(stats)
+    send_resp(conn, 200, body)
   end
 
   match _ do
-    send_resp(conn, 404, "oops")
+    send_resp(conn, 404, "unknown route!")
   end
 end
